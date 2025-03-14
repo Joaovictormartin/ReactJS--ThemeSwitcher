0b4abd7af786e62536a08a6cf6cbd2c0cc5ae490
Joao Victor <devjoaovictor10@gmail.com>
2025-03-14 14:01:55 -0300

    feat: arquivo de theme lightTheme e darkTheme

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,23 @@
+export const lightTheme = {
+  colors: {
+    title: "#000000",
+    background: "#f1f1f1",
+
+    gray: "#707070",
+    white: "#FFFFFF",
+    yello: "#f1c40f",
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    title: "#ffffff",
+    background: "#18181B",
+
+    gray: "#707070",
+    white: "#FFFFFF",
+    yello: "#f1c40f",
+  },
+};
+
+export type ThemeType = typeof lightTheme;
